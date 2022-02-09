427c9f778297ec61e7634b4f7d55eb6367d682a1
Vlad <koroliuk.work@gmail.com>
2022-02-09 12:58:58 +0200

    review main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import VueMoment from "vue-moment";
 import Vuesax from "vuesax/dist/vuesax.common.js";
 import SimpleVueValidation from "simple-vue-validator";
+import theme from "./vs-theme.js";
 import "./assets/scss/index.scss";
 import "vuesax/dist/vuesax.css";
 import "./plugins/axios";
@@ -12,23 +13,11 @@ import "boxicons/css/boxicons.css";
 import "fontisto/css/fontisto/fontisto.css";
 import "material-icons/iconfont/material-icons.css";
 
-Vue.config.productionTip = false;
-
-Vue.use(Vuesax, {
-  theme: {
-    colors: {
-      primary: "#db4c3f",
-      success: "rgb(23, 201, 100)",
-      danger: "rgb(242, 19, 93)",
-      warning: "rgb(255, 130, 0)",
-      dark: "rgb(36, 33, 69)",
-    },
-  },
-});
-
+Vue.use(Vuesax, theme);
 Vue.use(SimpleVueValidation);
 Vue.use(VueMoment);
 Vue.config.devtools = true;
+Vue.config.productionTip = false;
 
 new Vue({
   router,
---
+++ src/vs-theme.js
@@ -0,0 +1,11 @@
+export default {
+  theme: {
+    colors: {
+      primary: "#db4c3f",
+      success: "rgb(23, 201, 100)",
+      danger: "rgb(242, 19, 93)",
+      warning: "rgb(255, 130, 0)",
+      dark: "rgb(36, 33, 69)",
+    },
+  },
+};
